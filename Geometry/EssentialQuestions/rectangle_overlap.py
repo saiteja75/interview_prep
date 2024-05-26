0984bcfbbae39d6fa5eef5cc117863e1a0c15125
saiteja <saiteja.kasam75@gmail.com>
2024-05-26 13:13:32 +0530

    Geometry Problems

---
+++ Geometry/EssentialQuestions/rectangle_overlap.py
@@ -0,0 +1,15 @@
+# rectangle-overlap
+# https://leetcode.com/problems/rectangle-overlap/description/
+'''
+
+An axis-aligned rectangle is represented as a list [x1, y1, x2, y2], where (x1, y1) is the coordinate of its bottom-left corner, and (x2, y2) is the coordinate of its top-right corner. Its top and bottom edges are parallel to the X-axis, and its left and right edges are parallel to the Y-axis.
+
+Two rectangles overlap if the area of their intersection is positive. To be clear, two rectangles that only touch at the corner or edges do not overlap.
+
+Given two axis-aligned rectangles rec1 and rec2, return true if they overlap, otherwise return false.
+'''
+
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+       return rec1[0] < rec2[2] and rec2[0] < rec1[2] and rec1[1] < rec2[3] and rec2[1] < rec1[3]
+        
---
+++ Geometry/RecommendedQuestions/rectangle_area.py
@@ -0,0 +1,28 @@
+# rectangle-area
+# https://leetcode.com/problems/rectangle-area/description/
+''' 
+
+Given the coordinates of two rectilinear rectangles in a 2D plane, return the total area covered by the two rectangles.
+
+The first rectangle is defined by its bottom-left corner (ax1, ay1) and its top-right corner (ax2, ay2).
+
+The second rectangle is defined by its bottom-left corner (bx1, by1) and its top-right corner (bx2, by2).
+
+
+'''
+
+class Solution:
+    def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
+        # Compute the area of the two rectangles
+        area1 = (ax2 - ax1) * (ay2 - ay1)
+        area2 = (bx2 - bx1) * (by2 - by1)
+        
+        # Compute the overlap area
+        overlapWidth = min(ax2, bx2) - max(ax1, bx1)
+        overlapHeight = min(ay2, by2) - max(ay1, by1)
+        overlapArea = max(overlapWidth, 0) * max(overlapHeight, 0)
+        
+        # Compute the total area covered by the two rectangles
+        totalArea = area1 + area2 - overlapArea
+        
+        return totalArea
